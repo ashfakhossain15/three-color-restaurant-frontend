b6cd479b12779e43fadb38a4e0698d04abe1fce4
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-01-27 23:37:31 +0100

    fix something to deploy vercel

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,8 @@
+"use client";
 import React from "react";
 import Header from "../Components/Header/header";
 
-
-export default function Home({}: Props) {
+export default function Home() {
   return (
     <div>
       <Header />
