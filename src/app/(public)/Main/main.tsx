39a2caf99813d9f0008b7b190bf950596acffdab
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-01-29 21:28:24 +0100

    bg image position fix and opacity fixed

--- src/app/(public)/Main/main.tsx
+++ src/app/(public)/Main/main.tsx
@@ -2,7 +2,13 @@
 
 const Main = () => {
   return (
-    <div className="bg-[url(/trecolori-limhamn.jpg)] bg-repeat bg-cover h-screen bg-center"> home</div>
+    <div className="relative h-screen bg-cover bg-center bg-fixed bg-[url('/trecolori-limhamn.jpg')]">
+      <span className="absolute inset-0 bg-black bg-opacity-50"></span>
+
+      <div className="relative flex items-center justify-center h-full text-white text-2xl">
+        Home
+      </div>
+    </div>
   );
 };
 
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -6,7 +6,7 @@ import Main from "./Main/main";
 export default function Home() {
   return (
     <div>
-      <div className="absolute top-0 w-full">
+      <div className="absolute top-0 w-full z-50">
         <Header />
       </div>
       <Main />
