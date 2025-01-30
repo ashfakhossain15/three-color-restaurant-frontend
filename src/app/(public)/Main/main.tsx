e86002e86df01fa198f00c00a88c49ca4375afc1
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-01-30 17:35:10 +0100

    dashboard page bg image zoom fixed

--- src/app/(public)/Main/main.tsx
+++ src/app/(public)/Main/main.tsx
@@ -2,7 +2,7 @@
 
 const Main = () => {
   return (
-    <div className="relative h-screen bg-cover bg-center bg-fixed bg-[url('/trecolori-limhamn.jpg')]">
+    <div className="relative h-screen sm:bg-contain lg:bg-cover  bg-center bg-fixed bg-[url('/trecolori-limhamn.jpg')]">
       <span className="absolute inset-0 bg-black bg-opacity-50"></span>
 
       <div className="relative flex items-center justify-center h-full text-white text-2xl">
