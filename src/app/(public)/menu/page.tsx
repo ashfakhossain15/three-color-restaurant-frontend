0b9d57586c2effbe12cd37ec912ba940bbec75ca
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-02-17 21:01:42 +0100

    added shadcn for this project

--- src/app/(public)/menu/page.tsx
+++ src/app/(public)/menu/page.tsx
@@ -5,12 +5,12 @@ const Menu = () => (
   <div className="">
     <Header />
     <section className="relative h-full  bg-cover  bg-center bg-no-repeat bg-fixed bg-[url('/menu-img.jpg')] w-full">
-      <span className="absolute inset-0 bg-black bg-opacity-50 "></span>
-      <div className="w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center">
+      <div className="w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center opacity-100">
         <button className="bg-yellow-300 px-5 py-3 rounded-full">
           Order online
         </button>
       </div>
+      <span className="absolute inset-0 bg-black bg-opacity-50 "></span>
     </section>
     <Footer />
   </div>
