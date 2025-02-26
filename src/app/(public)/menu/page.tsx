bbe38d4aa4b244917876c77149fe796b49c87d17
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-02-26 20:26:51 +0100

    fixed opacity issue on menu page

--- src/app/(public)/menu/page.tsx
+++ src/app/(public)/menu/page.tsx
@@ -1,16 +1,19 @@
 import Footer from "@/app/Components/Footer/Footer";
 import Header from "@/app/Components/Header/header";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Menu = () => (
   <div className="">
     <Header />
     <section className="relative h-full  bg-cover  bg-center bg-no-repeat bg-fixed bg-[url('/menu-img.jpg')] w-full">
-      <div className="w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center opacity-100">
-        <button className="bg-yellow-300 px-5 py-3 rounded-full">
+      <div className="w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center opacity-100 z-50">
+        <Link href>
+        <Button className="bg-yellow-300 px-5 py-3 rounded-full">
           Order online
-        </button>
+        </Button></Link>
       </div>
-      <span className="absolute inset-0 bg-black bg-opacity-50 "></span>
+      <span className="absolute inset-0 bg-black bg-opacity-50 z-10 "></span>
     </section>
     <Footer />
   </div>
