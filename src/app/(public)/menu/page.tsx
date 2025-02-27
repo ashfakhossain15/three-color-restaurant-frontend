b848028030c6657f5b81562f777a9221d8787878
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-02-27 21:59:01 +0100

    fixed menu page button

--- src/app/(public)/menu/page.tsx
+++ src/app/(public)/menu/page.tsx
@@ -8,10 +8,11 @@ const Menu = () => (
     <Header />
     <section className="relative h-full  bg-cover  bg-center bg-no-repeat bg-fixed bg-[url('/menu-img.jpg')] w-full">
       <div className="w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center opacity-100 z-50">
-        <Link href>
-        <Button className="bg-yellow-300 px-5 py-3 rounded-full">
-          Order online
-        </Button></Link>
+        <Link href="https://open-vsx.org/extension/Equinusocio/vsc-material-theme">
+          <Button className="bg-yellow-300 px-5 py-3 rounded-full">
+            Order online
+          </Button>
+        </Link>
       </div>
       <span className="absolute inset-0 bg-black bg-opacity-50 z-10 "></span>
     </section>
