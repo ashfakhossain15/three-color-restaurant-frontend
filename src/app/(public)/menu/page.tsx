f819046f3df4f5459656c04ee5f0c4184155d932
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-02-28 23:35:31 +0100

    fixed menu page background opacity

--- src/app/(public)/menu/page.tsx
+++ src/app/(public)/menu/page.tsx
@@ -7,6 +7,8 @@ const Menu = () => (
   <div className="">
     <Header />
     <section className="relative h-full  bg-cover  bg-center bg-no-repeat bg-fixed bg-[url('/menu-img.jpg')] w-full">
+      <span className="absolute inset-0 bg-black bg-opacity-[0.3] z-10 "></span>
+
       <div className="w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center opacity-100 z-50">
         <Link href="https://open-vsx.org/extension/Equinusocio/vsc-material-theme">
           <Button className="bg-yellow-300 px-5 py-3 rounded-full">
@@ -14,7 +16,6 @@ const Menu = () => (
           </Button>
         </Link>
       </div>
-      <span className="absolute inset-0 bg-black bg-opacity-50 z-10 "></span>
     </section>
     <Footer />
   </div>
