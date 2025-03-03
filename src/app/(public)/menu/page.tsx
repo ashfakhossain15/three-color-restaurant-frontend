f51ee678c8c18199f9fc32f56d938119e32e420a
Ashfak Hossain <ashfaksikder15@gmail.com>
2025-03-03 18:26:21 +0100

    fixed menu page header access

--- src/app/(public)/menu/page.tsx
+++ src/app/(public)/menu/page.tsx
@@ -4,14 +4,14 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const Menu = () => (
-  <div className="">
+  <div>
     <Header />
     <section className="relative h-full bg-cover bg-center bg-no-repeat bg-fixed bg-[url('/menu-img.jpg')] w-full">
-      <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-50 "></div>
 
-      <div className="relative z-50 w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center">
+      <div className="relative  w-full mx-auto py-32 px-11 md:pt-40 flex flex-col justify-center items-center">
         <Link href="https://open-vsx.org/extension/Equinusocio/vsc-material-theme">
-          <Button className="bg-yellow-300 px-5 py-3 text-black rounded-full">
+          <Button className="bg-yellow-300 px-5 py-3 transition-all duration-500 hover:text-white text-black rounded-full">
             Order online
           </Button>
         </Link>
